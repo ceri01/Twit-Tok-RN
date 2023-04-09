592620eda13031801dbec77c1a21a1ea7a83cec1
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-04-10 00:48:46 +0200

    WIP: Profile interface

--- view/Profile.jsx
+++ view/Profile.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from "react";
-import {FlatList, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View} from "react-native";
+import {FlatList, Platform, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View} from "react-native";
 import UserView from "./user/UserView";
 import UserPicture from "./user/UserPicture";
 import UserName from "./user/UserName";
@@ -8,7 +8,13 @@ import CustomTextModal from "./modal/CustomTextModal";
 
 import {getProfile, setNewProfileName} from "../viewmodel/ProfileUserHandler"
 import {createPictureSource} from "../viewmodel/PictureHandler";
+import ProfilePicture from "./profile/ProfilePicture";
+import ProfileName from "./profile/ProfileName";
+import ProfileView from "./profile/ProfileView";
+import NativeStatusBarManagerIOS from "react-native/Libraries/Components/StatusBar/NativeStatusBarManagerIOS";
+import NativeStatusBarManager from "react-native/Libraries/Components/StatusBar/NativeStatusBarManagerIOS";
 import database from "../model/DBManager";
+import DBManager from "../model/DBManager";
 
 const DATA = [
     {"id": 1, name: "Mimmo"},
@@ -31,82 +37,46 @@ const DATA = [
     {"id": 18, name: "Nicola"},
 ]
 
-function Profile() {
-    const [modalNameVisible, setModalNameVisible] = useState(false);
-    // const [modalPicVisible, setModalPicVisible] = useState(false);
+function Profile({route}) {
     const reset = useRef(false); // flag to reset page
     const [ready, setReady] = useState(false);
     let profile = useRef(null)
 
+
+    function profileStyle() {
+        return ({
+                flex: 1,
+                flexDirection: "column",
+            }
+        )
+    }
+
     if (profile.current === null) {
-        database().getProfileFromDB((resultQuery) => {
+        console.log("kek")
+        DBManager.getInstance().getProfileFromDB((resultQuery) => {
+            console.log("aa" + resultQuery)
             profile.current = resultQuery
             setReady(true)
         }, (error) => {
-            console.log(error);
+            console.log("a" + error);
         })
 
-    }
-    const handleSliderReset = () => {
-        reset.current = false;
     }
 
     if (ready) {
         return (
-            <SafeAreaView>
+            <SafeAreaView style={profileStyle()}>
                 <StatusBar barStyle="light-content" backgroundColor="#6200ee"/>
                 <View style={style.profile}>
-                    <UserPicture changePicDimension={true} source={profile.current.picture}></UserPicture>
-                    <View style={style.usernameView}>
-                        <View style={style.username}>
-                            <UserName isProfileName={true} userName={profile.current.name}/>
-                            <View style={style.kek}>
-                                <View>
-                                    <Pressable style={style.editButton}
-                                               onPress={() => {
-                                                   setModalNameVisible(true)
-                                               }
-                                               }>
-                                        <View style={style.button}>
-                                            <CustomTextModal visibility={modalNameVisible}
-                                                             onChangeVisibility={setModalNameVisible}
-                                                             text={profile.current.name}
-                                                             onChangeText={setNewProfileName}
-                                                             isReset={reset}
-                                                             onReset={handleSliderReset}
-                                                             isName={true}>
-                                            </CustomTextModal>
-                                            <Icon name="edit" size={20} color="white"></Icon>
-                                        </View>
-                                    </Pressable>
-                                </View>
-                                {/*                            <View>
-                                <Pressable style={style.editButton}
-                                           onPress={() => {
-                                               setModalNameVisible(true)
-                                           }
-                                           }>
-                                    <View style={style.button}>
-                                        <CustomTextModal visibility={modalNameVisible}
-                                                         onChangeVisibility={setModalNameVisible}
-                                                         text={getProfileName()}
-                                                         onChangeText={setUsername}
-                                                         isReset={reset}
-                                                         onReset={handleSliderReset}
-                                                         isName={true}>
-                                        </CustomTextModal>
-                                        <Icon name="edit" size={20} color="white"></Icon>
-                                    </View>
-                                </Pressable>
-                            </View>*/}
-                            </View>
-                        </View>
-                    </View>
+                    <ProfileView profileName={profile.current.name} profilePicture={profile.current.picture}></ProfileView>
+                </View>
+                <View style={style.followed}>
+
                 </View>
                 {/*            <View>
                 <FlatList data={DATA}
                           renderItem={(element) => {
-                              return <UserView data={element.item} isProfile={true}/>
+                              return <ProfileView data={element.item} isProfile={true}/>
                           }}
                           keyExtractor={(element) => element.id}>
                 </FlatList>
@@ -126,70 +96,19 @@ function Profile() {
 
 const style = StyleSheet.create({
     profile: {
-        height: 150,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row"
-    },
-    usernameView : {
         flex: 1,
-        flexDirection: "column",
-        alignItems: "center",
         justifyContent: "center",
+        borderBottomWidth: 1,
+        borderRadius: 30
     },
-    editButton: {
-        flex: 0.1,
-        paddingHorizontal: 5
+    followed: {
+        flex: 6,
     },
-    username: {
+    waiting: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        paddingHorizontal: 5
     },
-    button: {
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#6200ee",
-        height: 30,
-        width: 30,
-        borderRadius: 5
-    },
-    modalView: {
-        margin: 10,
-        backgroundColor: "white",
-        borderRadius: 20,
-        paddingHorizontal: 25,
-        paddingTop: 25,
-        paddingBottom: 10,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    modalCenteredView: {
-        flex: 1,
-        backgroundColor: "rgba(0,0,0,0.5)",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    modalButtons: {
-        paddingTop: 20,
-        flexDirection: "row"
-    },
-    followedList: {
-        flex: 1,
-    },
-    text: {
-        fontSize: 25,
-        textAlign: "center"
-    }, kek: undefined
-
 });
 
 export default Profile;
