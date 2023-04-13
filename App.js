77818205d894acd4b60735673300a121d303375a
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-04-13 23:08:08 +0200

    Removed console log

--- App.js
+++ App.js
@@ -17,7 +17,6 @@ function App() {
     const load = useRef("")
     const [isLoading, setIsLoading] = useState(true);
 
-    console.log(Constants.expoConfig.extra.sentry_dsn)
     if (Constants.expoConfig.extra.sentry_dsn !== undefined && Constants.expoConfig.extra.sentry_dsn !== null) {
         Sentry.init({
             dsn: Constants.expoConfig.extra.sentry_dsn,
