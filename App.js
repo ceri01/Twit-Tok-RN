e22edf9e057bd5532679cdaf2a52dfa3959741e2
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-05-29 21:55:10 +0200

    Removed useless import, semicolons and console.log

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React, {useEffect, useRef, useState} from "react";
-import Register from "./view/Register";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import Main from "./view/Main";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {Button, NativeModules, SafeAreaView, StyleSheet, Text, View} from "react-native";
-import UtilityStorageManager from "./model/UtilityStorageManager";
-import {initApplication, initEnvironment, reloadApp} from "./viewmodel/initApp";
-import {NavigationContainer} from "@react-navigation/native";
-import {checkConnection} from "./viewmodel/ConnectionHandler";
+import React, {useEffect, useRef, useState} from "react"
+import Register from "./view/Register"
+import {SafeAreaProvider} from "react-native-safe-area-context"
+import Main from "./view/Main"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {Button, NativeModules, SafeAreaView, StyleSheet, Text} from "react-native"
+import UtilityStorageManager from "./model/UtilityStorageManager"
+import {initEnvironment, reloadApp} from "./viewmodel/initApp"
+import {NavigationContainer} from "@react-navigation/native"
+import {checkConnection} from "./viewmodel/ConnectionHandler"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 function App() {
     const load = useRef("")
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true)
     const [online, setOnline] = useState(true)
 
     useEffect(() => {
         checkConnection(setOnline)
-    });
+    })
 
     if (!online) {
         return (
@@ -26,32 +26,32 @@ function App() {
                 <Text style={{fontSize: 25, fontStyle: "italic"}}>Connection error. Is not possible to retrieve data of followed users, please check your connection and retry. Try to click button below or restart app</Text>
                 <Button title="Reload" onPress={() => {
                     reloadApp().then(() => {
-                        NativeModules.DevSettings.reload();
+                        NativeModules.DevSettings.reload()
                     })
                 }}/>
             </SafeAreaView>
-        );
+        )
     } else {
         if (isLoading) {
             UtilityStorageManager.isFirstStart().then((res) => {
                 if (res) {
-                    load.current = "Register";
+                    load.current = "Register"
                     setIsLoading(false)
                 } else {
                     initEnvironment().then(() => { // if isn't first start this line is not executed
-                        load.current = "Main";
+                        load.current = "Main"
                         setIsLoading(false)
-                    }).catch(() => {
-                        console.log("skip init environment")
+                    }).catch((err) => {
+                        console.log(err)
                     })
                 }
             }).catch((err) => {
-                console.log("isFirstStart " + err)
+                console.log(err)
             })
 
             return (
                 <Text style={style.loading}>Loading...</Text>
-            );
+            )
         } else {
             return (
                 <SafeAreaProvider>
@@ -80,4 +80,4 @@ const style = StyleSheet.create({
     }
 })
 
-export default App;
+export default App
